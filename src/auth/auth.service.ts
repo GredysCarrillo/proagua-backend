e5720aa7b446629640505917a50da9b96b5fb041
Anner Escobar <70228927+AnnerEscobar@users.noreply.github.com>
2024-09-01 18:52:02 -0600

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class AuthService {
+  create(createAuthDto: CreateUserDto) {
+    return 'This action adds a new auth';
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateUserDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
